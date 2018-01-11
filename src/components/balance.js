7f8fe89dce1e913183f01d0bf3d36e5f737ba379
Niten <niten2@yandex.ru>
2018-01-11 12:26:58 +0700

    edit react

--- src/components/balance.js
+++ src/components/balance.js
@@ -1,8 +1,8 @@
 import React, { Component } from 'react'
 import contract from 'truffle-contract'
-import SampleStorageContract from '../build/contracts/SampleCrowdsale.json'
-import SampleCrowdsaleToken from '../build/contracts/SampleCrowdsaleToken.json'
-import getWeb3 from './utils/getWeb3'
+import SampleStorageContract from 'build/contracts/SampleCrowdsale.json'
+import SampleCrowdsaleToken from 'build/contracts/SampleCrowdsaleToken.json'
+import getWeb3 from 'src/utils/getWeb3'
 
 export default class Balance extends Component {
 
@@ -25,38 +25,21 @@ export default class Balance extends Component {
     this.setState({ web3, sampleStorageContract, sampleCrowdsaleToken })
   }
 
-  async handle() {
+  handle = async () => {
     let { sampleStorageContract, sampleCrowdsaleToken, web3 } = this.state
 
-    // let tokenAddress = await contract.token()
+    let account1 = web3.eth.accounts[1]
 
-    // let instanceToken = await token.at(tokenAddress)
+    let instanceSampleStorageContract = await sampleStorageContract.deployed()
+    let tokenAddress = await instanceSampleStorageContract.token()
+    let instanceSampleCrowdsaleToken = await sampleCrowdsaleToken.at(tokenAddress)
 
-    // console.log(instanceToken)
-    // instanceToken.balanceOf
+    let balance = (await instanceSampleCrowdsaleToken.balanceOf(account1)).toString(10)
 
-    // let instanceToken = await contract.deployed()
+    console.log(balance)
 
+    this.setState({ balance })
 
-
-    // await Promise.new((resolve, reject) => {
-
-      // web3.eth.getAccounts(async (error, accounts) => {
-      //   let account1 = accounts[1]
-
-
-          // let tokenAddress = await instance.token()
-          // let sampleCrowdsaleTokenInstance = sampleCrowdsaleToken.at(tokenAddress)
-          // let balance = await sampleCrowdsaleTokenInstance.balanceOf(account1)
-          // let res = web3.fromWei(balance.toString(10), "ether")
-
-          // console.log("result ether by contract")
-          // console.log(res)
-        // })
-      // })
-
-
-    })
   }
 
   render() {
@@ -64,19 +47,9 @@ export default class Balance extends Component {
 
     return (
       <div className="App">
-        <nav className="navbar pure-menu pure-menu-horizontal">
-            <a href="#" className="pure-menu-heading pure-menu-link">Truffle Box</a>
-        </nav>
-
-        <main className="container">
-          <div className="pure-g">
-            <div className="pure-u-1-1">
-              <h1>Your balance contract { balance || "not found" }</h1>
-              <button onClick={this.handle}> refresh </button>
-            </div>
-          </div>
-        </main>
+        <h1>Your balance account1 { balance || "not found" }</h1>
+        <button onClick={ this.handle }> refresh </button>
       </div>
-    );
+    )
   }
 }
--- src/utils/getWeb3.js
+++ src/utils/getWeb3.js
@@ -1,10 +1,12 @@
 import Web3 from 'web3'
 
+const httpUrl = 'http://127.0.0.1:9545'
+
 let getWeb3 = new Promise(function(resolve, reject) {
 
   window.addEventListener('load', function() {
-    var results
-    var web3 = window.web3
+    let web3 = window.web3
+    // var results
 
     // // Checking if Web3 has been injected by the browser (Mist/MetaMask)
     // if (typeof web3 !== 'undefined') {
@@ -22,20 +24,23 @@ let getWeb3 = new Promise(function(resolve, reject) {
 
     //   // Fallback to localhost if no web3 injection. We've configured this to
     //   // use the development console's port by default.
-      var provider = new Web3.providers.HttpProvider('http://127.0.0.1:9545')
+      var provider = new Web3.providers.HttpProvider(httpUrl)
     //   // var provider = new Web3.providers.HttpProvider('http://127.0.0.1:7545')
     //   console.log(111)
       // var provider = new Web3.providers.HttpProvider('http://127.0.0.1:8545')
 
       web3 = new Web3(provider)
 
-      results = {
-        web3: web3
-      }
+      // results = {
+      //   web3: web3
+      // }
 
       console.log('No web3 instance injected, using Local web3.');
 
-      resolve(results)
+      // resolve(results)
+
+      resolve(web3)
+
     // }
   })
 })
