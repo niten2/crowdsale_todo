b96a310979984e4b5c98b166aa8189b75f093ec5
Niten <niten2@yandex.ru>
2018-01-11 17:31:56 +0700

    add base spec

--- test/simplestorage.js
+++ test/simplestorage.js
@@ -1,17 +1,25 @@
-import { expect } from "chai"
+import chai, { expect } from "chai"
+import chaiAsPromised from "chai-as-promised"
+
+chai.use(chaiAsPromised)
+
 let SampleCrowdsale = artifacts.require("./SampleCrowdsale.sol")
 
 contract('SampleCrowdsale', (accounts) => {
+  let cap = 20
 
-  it("should", async () => {
+  it(`should have capacity ${cap}`, async () => {
+    let account1 = web3.eth.accounts[1]
     let instance = await SampleCrowdsale.deployed()
 
+    await instance.sendTransaction({ from: account1, value: web3.toWei(cap, "ether")})
 
-    let addr = await instance.token()
-
-    expect(addr).to.be.a("string")
-
-    console.log(addr)
+    try {
+      await instance.sendTransaction({ from: account1, value: web3.toWei(1, "ether")})
+      throw new Error("should return throw")
+    } catch (err) {
+      expect(err.message).eql("VM Exception while processing transaction: revert")
+    }
 
   })
 
